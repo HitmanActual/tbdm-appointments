d0743c25163bcb87a4bdbe85d1070882d9e3ae10
HitmanActual <dev.render@gmail.com>
2020-05-18 01:23:47 +0200

    deleted at and handler

--- database/migrations/2020_05_16_114137_create_appointments_table.php
+++ database/migrations/2020_05_16_114137_create_appointments_table.php
@@ -22,6 +22,7 @@ class CreateAppointmentsTable extends Migration
             $table->integer('queue_number')->unsigned()->nullable();
             $table->enum('status',['confirmed','cancelled']);
             $table->timestamps();
+            $table->timestamp('deleted_at')->nullable();
         });
     }
 
